1422599435d6871469f28f798f5ffb41ae284987
Akshay Ganesh <akshayiyerg@gmail.com>
2025-04-04 20:12:15 -0400

    Building PokemonDetail Screen

---
+++ PokeDex/PokeDex/PokemonDetail.swift
@@ -0,0 +1,18 @@
+//
+//  PokemonDetail.swift
+//  PokeDex
+//
+//  Created by Akshay  on 2025-04-09.
+//
+
+import SwiftUI
+
+struct PokemonDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PokemonDetail()
+}
