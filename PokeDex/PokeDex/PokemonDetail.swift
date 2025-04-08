58f4d80632bbe5bf3a5f8e0724a74e1fc1a386fd
Akshay Ganesh <akshayiyerg@gmail.com>
2025-04-07 20:46:53 -0400

    Image and Types are added to the PokemonDetail screen

--- PokeDex/PokeDex/PokemonDetail.swift
+++ PokeDex/PokeDex/PokemonDetail.swift
@@ -22,6 +22,32 @@ struct PokemonDetail: View {
                     .resizable()
                     .scaledToFit()
                     .shadow(color: .black ,radius: 6)
+                
+                AsyncImage(url: pokemon.sprite) { image in
+                    image
+                        .interpolation(.none)
+                        .resizable()
+                        .scaledToFit()
+                        .padding(.top, 50)
+                        .shadow(color: .black, radius: 6)
+                } placeholder: {
+                    ProgressView()
+                }
+            }
+            
+            HStack {
+                ForEach(pokemon.types!, id: \.self) { type in
+                    Text(type.capitalized)
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.black)
+                        .shadow(color: .white, radius: 1)
+                        .padding(.vertical, 7)
+                        .padding(.horizontal)
+                        .background(Color(type.capitalized))
+                        .clipShape(.capsule)
+                        
+                }
             }
         }
         .navigationTitle(pokemon.name!.capitalized)
