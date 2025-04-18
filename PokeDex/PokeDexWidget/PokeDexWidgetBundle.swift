b194bfaa85e8c9ff15976c164b4bf82ff3f04158
Akshay Ganesh <akshayiyerg@gmail.com>
2025-04-17 20:06:25 -0400

    Widget franework added to the project

---
+++ PokeDex/PokeDexWidget/PokeDexWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  PokeDexWidgetBundle.swift
+//  PokeDexWidget
+//
+//  Created by Akshay  on 2025-04-17.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct PokeDexWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        PokeDexWidget()
+    }
+}
